774c71b3f9c25810aae23a2f74571a7509c2bce7
Brian  Dejesus <briandejesus@Brians-MBP.attlocal.net>
2017-09-30 20:58:40 -0700

    Add author

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ class Header extends React.Component {
   render() {
     return (
     <div className="App-header">
+      <p className="author"> by <a href="https://briandejesus.github.io/">Brian DeJesus</a> </p>
       <h1>Binary Adder-Subtractor</h1>
     </div>
   );
